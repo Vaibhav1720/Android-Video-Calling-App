3a919a4547ec99668cd64d9cd9efb9570fe1b406
Vaibhav Singh <vaibhavsingh1720@gmail.com>
2021-04-26 17:38:34 +0530

    First commit

---
+++ app/src/main/java/com/example/videocaller/SignupActivity.java
@@ -0,0 +1,93 @@
+package com.example.videocaller;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    EditText emailBox, passwordBox,nameBox;
+    Button loginBtn, signupBtn;
+
+    FirebaseFirestore database;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        database = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        emailBox = findViewById(R.id.emailBox);
+        passwordBox = findViewById(R.id.passwordBox);
+        nameBox = findViewById(R.id.namebox);
+
+        loginBtn = findViewById((R.id.loginbtn));
+        signupBtn = findViewById(R.id.createbtn);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, pass, name;
+                email = emailBox.getText().toString();
+                pass = passwordBox.getText().toString();
+                name = nameBox.getText().toString();
+
+                User user = new User();
+                user.setEmail(email);
+                user.setName(name);
+                user.setPass(pass);
+
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                            database.collection("Users")
+                                    .document().set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                }
+                            });
+
+                            Toast.makeText(SignupActivity.this,  "Account is created", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText( SignupActivity.this, task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+    });
+}
+}
+
+
+
+
+
+
+
+
+
+
+
